6aeee245199ea8444d0025e1601d32c832247dc3
whypxralich1 <maxgliantsev@gmail.com>
2025-04-03 12:07:17 +0300

    обновлен к лабе 5 (аннотации)
    обновлен к лабе 5 (аннотации)

---
+++ sem2_laba5/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class DemoApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        VehicleDisplay vehicleDisplay = context.getBean(VehicleDisplay.class);
+        vehicleDisplay.display();
+
+        vehicleDisplay.cleanup();
+    }
+}
+ 
